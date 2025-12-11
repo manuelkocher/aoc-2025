8e109cd37ccaf61fd561780c89a1db2bed94101a
Manuel Kocher <manuelkocher98@gmail.com>
2025-12-11 09:44:17 +0100

    Add day 07 part 1 solution

---
+++ 07/01.rs
@@ -0,0 +1,59 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+// read a file by lines
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+  let file = File::open(filename)?;
+  Ok(io::BufReader::new(file).lines())
+}
+
+fn find_S(line: &str) -> usize
+{
+  for (i, c) in line.chars().enumerate() {
+    if c == 'S' {
+      return i;
+    }
+  }
+  return 0;
+}
+
+fn main() -> io::Result<()> {
+  let mut num_splits: i64 = 0; // number of splits
+  let input: &str = "./input.txt"; // file name of input
+
+  let mut beam_idxs: Vec<usize> = Vec::new();
+
+  if let Ok(lines) = read_lines(input) {
+    for (i, line) in lines.map_while(Result::ok).enumerate() {
+      if i == 0 {
+        beam_idxs.push(find_S(&line));
+      } else {
+        // go over all beams and split whenever a ^ is present
+        let new_beam_idxs: Vec<usize> = Vec::new();
+        for idx in beam_idxs.clone() {
+          let c = line.chars().nth(idx).unwrap();
+          if c == '^' {
+            beam_idxs.push(idx - 1);
+            beam_idxs.push(idx + 1);
+            num_splits += 1;
+            //println!("push {} {}, remove {:?}", idx - 1, idx + 1, beam_idxs.iter().position(|n| *n == idx));
+            beam_idxs.remove(beam_idxs.iter().position(|n| *n == idx).unwrap());
+          }
+        }
+        beam_idxs.sort();
+        beam_idxs.dedup();
+        //println!("{:?}\n", beam_idxs);
+      }
+    }
+  }
+
+  println!("{}", num_splits);
+
+  // wait for user input
+  let mut buffer = String::new();
+  io::stdin().read_line(&mut buffer).unwrap();
+
+  Ok(())
+}
